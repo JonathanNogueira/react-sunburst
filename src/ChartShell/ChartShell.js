f77cc553b505420eb870cc86278367687d4fe4b1
JonathanNogueira <jonathan.nogueira001@gmail.com>
2017-05-02 22:39:41 -0400

    center svg content

--- src/ChartShell/ChartShell.js
+++ src/ChartShell/ChartShell.js
@@ -6,7 +6,7 @@ const ChartShell = ({ children, hasData, viewBox }) => {
     return (
         <div className='chart-shell'>
             { hasData ? 
-                ( <svg width='100%' height='100%' viewBox={ viewBox } preserveAspectRatio="xMinYMin meet">
+                ( <svg width='100%' height='100%' viewBox={ viewBox } preserveAspectRatio="xMidYMid meet">
                     { children }
                   </svg>  
                 ) : (
